7da01f95bc03aa7e2bf7934c1b6e2fc2430e9296
Aiman Md Uslim <amduslim@purdue.edu>
2017-05-17 00:03:46 +0800

    modified

---
+++ quran-tracker-api/models/ayat.py
@@ -0,0 +1,55 @@
+from db import db
+
+class AyatModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    surah = db.Column(db.String(80))
+    juz = db.Column(db.Integer)    
+    number = db.Column(db.Integer)
+    revisit_frequency = db.Column(db.Integer)
+    last_refreshed = db.Column(db.DateTime)
+    hifz_strength = db.Column(db.Integer)
+    note = db.Column(db.Text)
+
+
+
+
+
+    # name = db.Column(db.String(80))
+    # price = db.Column(db.Float(precision=2))
+
+    # store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    # store = db.relationship('StoreModel')
+
+    def __init__(self, surah, number, date_refreshed, hifz_strength, theme, note):
+        self.surah = surah
+        self.number = number
+        self.juz = self.lookup_juz(surah, number)
+        
+        self.revisit_frequency = 0
+        self.last_refreshed = date_refreshed
+        self.hifz_strength = hifz_strength
+        self.theme = theme
+        self.note = note
+
+
+
+    def json(self):
+        return {'surah': self.surah, 'juz': self.juz, 'number': self.number, 'revisit_frequency': self.revisit_frequency, 'last_refreshed': self.date_refreshed, 'hifz_strength': self.hifz_strength, 'theme': self.theme}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def find_by_surah_number(cls, surah, number):
+        return cls.query.filter_by(surah=surah, number=number)
+
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
---
+++ quran-tracker-api/resources/ayat.py
@@ -0,0 +1,153 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.ayat import AyatModel
+
+class Ayat(Resource):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument('surah',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+    parser.add_argument('number',
+        type=int,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+    parser.add_argument('theme',
+        type=str,
+        required=False,
+        help="This field is for you to describe the theme of the ayat"
+    )
+
+    parser.add_argument('note',
+        type=str,
+        required=False,
+        help="This field is for you to input notes for the ayat"
+    )
+
+    parser.add_argument('date_refreshed',
+        type=lambda x: datetime.strptime(x,'%Y-%m-%dT%H')),
+        required=False,
+        help="This field is to specify the last time this ayat was refreshed"
+    )
+
+    parser.add_argument('difficulty',
+        type=int,
+        required=False,
+        help="This field is for you to specify ayat's difficulty, range 0-5, from easiest to hardest"
+    )
+    
+
+
+
+
+
+    # parser.add_argument('price',
+    #     type=float,
+    #     required=True,
+    #     help="This field cannot be left blank!"
+    # )
+
+    # parser.add_argument('price',
+    #     type=float,
+    #     required=True,
+    #     help="This field cannot be left blank!"
+    # )
+    # parser.add_argument('store_id',
+    #     type=int,
+    #     required=True,
+    #     help="Every item needs a store_id."
+    # )
+
+    @jwt_required()
+    def get(self, surah, number):
+        ayat = AyatModel.find_by_surah_number(surah, number)
+        
+        if ayat:
+            return ayat.json()
+        return {'message': 'Item not found'}, 404
+
+        # item = ItemModel.find_by_name(name)
+        # if item:
+        #     return item.json()
+        # return {'message': 'Item not found'}, 404
+
+    def post(self, surah, number):
+        if AyatModel.find_by_surah_number(surah, number):
+            return {'message': "This ayat is already in database.", 400
+
+        data = Ayat.parser.parse_args()
+
+        ayat = AyatModel(data.get('surah'), 
+            data.get('number'),
+            data.get('date_refreshed'),
+            data.get('difficulty'),
+            data.get('theme'),
+            data.get('note')
+            )
+
+        try:
+            ayat.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the ayat."}, 500
+
+        return ayat.json(), 201        
+
+
+        # if ItemModel.find_by_name(name):
+        #     return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        # data = Item.parser.parse_args()
+
+        # item = ItemModel(name, data['price'], data['store_id'])
+
+        # try:
+        #     item.save_to_db()
+        # except:
+        #     return {"message": "An error occurred inserting the item."}, 500
+
+        # return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Ayat.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+
+        if item:
+            item.price = data['price']
+        else:
+            item = ItemModel(name, data['price'])
+
+        item.save_to_db()
+
+        return item.json()
+
+
+
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+
+        if item:
+            item.price = data['price']
+        else:
+            item = ItemModel(name, data['price'])
+
+        item.save_to_db()
+
+        return item.json()
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
