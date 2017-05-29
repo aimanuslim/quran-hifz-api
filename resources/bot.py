4247016d0dd0f66cccf0ebf639bfaf57c50a064e
Aiman Md Uslim <amduslim@purdue.edu>
2017-05-29 11:35:52 +0800

    added debug

--- resources/bot.py
+++ resources/bot.py
@@ -11,6 +11,7 @@ class Bot(Resource):
         # the 'hub.challenge' value it receives in the query arguments
         if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
             if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+                print("OS" + os.environ["VERIFY_TOKEN"])
                 return "Verification token mismatch", 403
             return request.args["hub.challenge"], 200
 
